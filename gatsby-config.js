37a7d22e8d27aba81ae4354024bc1049746b281b
crakoucas <crakoucas@gmail.com>
2018-11-02 12:21:54 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Mushdoom Reborn',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Mushdoom',
+        short_name: 'Mushdoom',
+        start_url: '/',
+        background_color: '#000000',
+        theme_color: '#000000',
+        display: 'minimal-ui',
+        //icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+}
---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const NotFoundPage = () => (
+  <Layout>
+    <h1>Demande A Bertin </h1>
+    <h2>Tu dois surrement aller en A</h2>
+  </Layout>
+)
+
+export default NotFoundPage
